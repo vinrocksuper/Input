a0c22e33e19f9ad5f4ab42fa546a8761a8a4899d
BT_1N3_18 <vli8558@bths.edu>
2018-09-24 10:06:50 -0400

    name + age v3 commented.
    Now guessing game

--- src/testfile.java
+++ src/testfile.java
@@ -1,8 +1,11 @@
 import java.util.Scanner;
 
+import static oracle.jrockit.jfr.events.Bits.intValue;
+
 public class testfile {
     public static void main(String [] args)
     {
+        /**
         Scanner input = new Scanner(System.in);
         System.out.println("What's your name?");
         String name = input.nextLine();
@@ -10,6 +13,20 @@ public class testfile {
         int age = input.nextInt();
         System.out.println("Hello, " + name + " who is " + age);
         input.close();
+        **/
 
+        Scanner input = new Scanner(System.in);
+        System.out.println("Guess a number between 1-100");
+        int num = intValue((Math.random()*100+1));
+        int guess = intValue(input.nextInt());
+        while(guess != num)
+        {
+            System.out.println("close, try again!");
+            guess = intValue(input.nextInt());
+        }
+        if(guess == num)
+        {
+            System.out.print("Congrats!");
+        }
     }
 }
