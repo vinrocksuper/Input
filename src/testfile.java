a0520912ceac138de315f63e2b7ac390fb4a43a9
BT_1N3_18 <vli8558@bths.edu>
2018-09-24 09:55:30 -0400

    name + age v3

--- src/testfile.java
+++ src/testfile.java
@@ -7,7 +7,7 @@ public class testfile {
         System.out.println("What's your name?");
         String name = input.nextLine();
         System.out.println("How old are you?");
-        Integer age = input.nextInt();
+        int age = input.nextInt();
         System.out.println("Hello, " + name + " who is " + age);
         input.close();
 
