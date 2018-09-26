4a77d33310f1e6f5524349e39e33f830847e96f0
BT_1N3_18 <vli8558@bths.edu>
2018-09-26 09:54:13 -0400

    Finished binary both games

--- src/testfile.java
+++ src/testfile.java
@@ -15,7 +15,7 @@ public class testfile {
          **/
 
         try (Scanner input = new Scanner(System.in)) {
-            System.out.println("Type 1 to guess, type anything else to let the AI guess");
+            System.out.println("Type 1 to guess, type any other number to let the AI guess");
             int choice = intValue(input.nextInt());
             if (choice ==1)
             {
@@ -23,47 +23,76 @@ public class testfile {
                 int num = intValue((Math.random() * 100 + 1));
                 int guess = intValue(input.nextInt());
                 int guessCount = 0;
-                while (guess != num) {
-                    System.out.println("Close, try again!");
-                    guess = intValue(input.nextInt());
-                    guessCount++;
+                System.out.println(Integer.toString(num));
+                while (guess != num)
+                {
+                    if(guess < num)
+                    {
+                        System.out.println("The number is greater than your guess");
+                        guess = intValue(input.nextInt());
+                        guessCount++;
+                    }
+                    if(guess > num)
+                    {
+                        System.out.println("The number is less than your guess");
+                        guess = intValue(input.nextInt());
+                        guessCount++;
+                    }
+
                 }
                 if (guess == num) {
                     guessCount++;
-                    System.out.print("Congrats! It took you " +guessCount + "guesses");
+                    System.out.print("Congrats! It took you " +guessCount + " guesses");
                 }
             }
             else
             {
-                System.out.println("Hello, I will be attempting to guess your number from 1-100");
+                System.out.println("Hello, I will be attempting to guess your number from 0-99");
                 System.out.println("If my guess is less than your number please type lower, if it is greater, type higher, and if correct, type correct");
                 double AIguess = 50;
                 boolean correct = false;
                 int min =0;
                 int max =100;
+                boolean firstGuess = true;
+                int guessCount = 0;
                 System.out.println("is your number " + AIguess);
                 while(correct == false)
                 {
                     String reply = input.next();
 
-                    if(reply.equalsIgnoreCase("lower"))
+                    if(reply.equalsIgnoreCase("lower") && firstGuess == true)
                     {
-                        AIguess = AIguess  - (.25 * (max-min));
                         max = intValue(AIguess);
+                        guessCount++;
+                        firstGuess = false;
+                    }
+                    if(reply.equalsIgnoreCase("higher") && firstGuess == true)
+                    {
+                        min = intValue(AIguess);
+                        guessCount++;
+                        firstGuess = false;
+                    }
+                    if(reply.equalsIgnoreCase("lower") && firstGuess == false)
+                    {
+                        max = intValue(AIguess);
+                        AIguess = AIguess  - (.5 * (max-min));
+                        guessCount++;
                         AIguess = intValue(AIguess);
+
                         System.out.println("is your number " + AIguess);
                     }
-                    else if(reply.equalsIgnoreCase("higher"))
+                    else if(reply.equalsIgnoreCase("higher") && firstGuess == false)
                     {
-
-                        AIguess = AIguess  + (.25 * (max-min));
                         min = intValue(AIguess);
+                        AIguess = AIguess  + (.5 * (max-min));
+                        guessCount++;
                         AIguess = intValue(AIguess);
+
                         System.out.println("is your number " + AIguess);
                     }
                     else if(reply.equalsIgnoreCase("correct"))
                     {
-                        System.out.println("Hurray");
+                        System.out.println("Hurray it took me " + guessCount +" tries.");
                         correct = true;
                     }
 
